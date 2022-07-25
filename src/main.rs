18553548603a11ff02ccaaaee8c6a5a2ad68cc4e
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-07-25 01:27:59 -0700

    Add working code to find BLAKE3, SHA-256 and SHA3-256 hashes of the current Cargo.toml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use sha2;
+use sha3::{Digest, Sha3_256};
+use std::fs;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut hasher_b2 = blake3::Hasher::new();
+    let mut file_b2 = fs::File::open("Cargo.toml").unwrap();
+
+    io::copy(&mut file_b2, &mut hasher_b2).unwrap();
+    let hash_bytes_b2 = hasher_b2.finalize();
+    println!("\nBlake3 Hash: {:?}", hash_bytes_b2);
+
+    let mut hasher = sha2::Sha256::new();
+    let mut file = fs::File::open("Cargo.toml").unwrap();
+
+    io::copy(&mut file, &mut hasher).unwrap();
+    let hash_bytes = hasher.finalize();
+    println!("\nSHA-256 Hash: {:?}", hex::encode(&hash_bytes));
+
+    let mut hasher3 = Sha3_256::new();
+    let mut file3 = fs::File::open("Cargo.toml").unwrap();
+
+    io::copy(&mut file3, &mut hasher3).unwrap();
+    let hash_bytes3 = hasher3.finalize();
+    let hex_string3 = hex::encode(&hash_bytes3);
+    println!("\nSHA3-256 Hash: {:?}", hex_string3);
 }
